21fd03da989d53f527b1ba55025f4ab2a448555b
Kunal Bamane <22bcs147@iiitdmj.ac.in>
2024-09-05 19:39:16 +0530

    removed all the waste navigation

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -2,8 +2,8 @@ import { redirect } from "next/navigation";
 
 const MainLayout = async ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-full bg-no-repeat relative dark:bg-gradient-to-t dark:from-[#1e1f21] dark:from-10% dark:via-[#292a2d] dark:via-50% dark:to-[#36373a] ">
-      <main className="md:pl-[72px] h-full relative z-10">{children}</main>
+    <div className="h-full relative">
+      <main className="child">{children}</main>
     </div>
   );
 };
--- components/navigation/navbar.tsx
+++ components/navigation/navbar.tsx
@@ -1,3 +1,3 @@
 export default function Page() {
-  return <div>navbar </div>;
+  return <div>navbar</div>;
 }
