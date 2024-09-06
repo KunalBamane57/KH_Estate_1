b5b15da802acb6926cd06900b7428c99b185bc37
Harsh Range <harshrange123@gmail.com>
2024-09-06 18:42:31 +0000

    db setup

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,8 +1,12 @@
 import { redirect } from "next/navigation";
+import Navbar from "@/components/navigation/navbar"
 
 const MainLayout = async ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full relative">
+      <div className="">
+        <Navbar />
+      </div>
       <main className="child">{children}</main>
     </div>
   );
